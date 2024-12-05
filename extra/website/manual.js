fc4864dfce0aee52441f4fbe654a663f0d159b27
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2024-12-05 01:31:14 +0100

    Add website

---
+++ extra/website/manual.js
@@ -0,0 +1,28 @@
+// This module contains the code to interpret the “manual.html” buttons.
+
+const buttonElements = document.querySelectorAll('button[data-action]')
+
+for (const buttonElement of buttonElements) {
+  const actionName = buttonElement.dataset.action
+
+  switch (actionName) {
+    case 'copyToClipboard':
+      buttonElement.addEventListener('click', () => {
+        navigator.clipboard
+          .writeText(buttonElement.value)
+          .then(() => {
+            buttonElement.textContent = buttonElement.dataset.copyFeedback
+            setTimeout(() => {
+              buttonElement.textContent = buttonElement.ariaLabel
+            }, 1000)
+          })
+      })
+      break
+
+    default:
+      console.error(
+        'Unknown action: "%s"',
+        actionName
+      )
+  }
+}
